d86d04590609d1fc96f52deb19fcd3db09fa0ee0
TUIzFPV <santipap350z@gmail.com>
2026-07-26 20:27:54 +0700

    Add files via upload

--- lib/site.ts
+++ lib/site.ts
@@ -1,4 +1,4 @@
 export const siteUrl =
-  process.env.NEXT_PUBLIC_SITE_URL?.replace(/\/$/, "") ?? "https://obixconfigfpv.onrender.com";
+  process.env.NEXT_PUBLIC_SITE_URL?.replace(/\/$/, "") ?? "https://obixconfig.vercel.app";
 
 export const siteHost = siteUrl.replace(/^https?:\/\//, "");
